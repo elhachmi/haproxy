a3f79b1025c3cfc0daa37e28e4c83a83159db9ee
elhachmi <elhachmi@gmail.com>
2018-11-12 16:54:37 +0000

    Update renew-cert-post-hook.sh

--- 1.8/alpine/renew-cert-post-hook.sh
+++ 1.8/alpine/renew-cert-post-hook.sh
@@ -1,5 +1,5 @@
 echo "\n\n\n\nConcatenating fullchain and private key in pem file .... "
-cat /etc/letsencrypt/live/dev-certificates/fullchain.pem /etc/letsencrypt/live/dev-certificates/privkey.pem  > /etc/letsencrypt/live/dev-certificates/certificate.pem 
+cat /etc/letsencrypt/live/certificates/fullchain.pem /etc/letsencrypt/live/certificates/privkey.pem  > /etc/letsencrypt/live/certificates/certificate.pem 
 echo "\n\n\n\nReloading Haproxy configuration..."
 /reload.sh &
 echo "\n\n\n\n Renwal done "
